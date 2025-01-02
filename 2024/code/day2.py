cf5e65574b4311a66c8ecbe1defc1dd4e577a0af
Jonas Gilbert <jonas.gilbert.0503@gmail.com>
2025-01-02 18:06:40 +0100

    day2 is now working

--- 2024/code/day2.py
+++ 2024/code/day2.py
@@ -12,26 +12,31 @@ def number_of_save_reports():
         is_decr_initial_value = level_difference_and_is_decreasing(numbers_list[0], numbers_list[1])[1]
         save_with_removed_number = False
         is_save = True
-        removed_one_number = False
 
         # iterate over one line
         for i in range(0, len(numbers_list)-1):
             difference, is_decr = level_difference_and_is_decreasing(numbers_list[i], numbers_list[i+1])
 
+            # one error
             if (difference >= 4 or difference <= 0) or (is_decr_initial_value != is_decr):
                 is_save = False
                 list_of_numbers = []
+                # all possibilities with one removed number
                 for i in range(0, len(numbers_list_original)):
                     numbers_list.pop(i)
                     list_of_numbers.append(numbers_list)
                     numbers_list = numbers_list_original.copy()
 
+                # iterate over the possibilities with one removed number
                 for l in list_of_numbers:
                     save_with_removed_number = True
+                    is_decr_initial_value = level_difference_and_is_decreasing(l[0], l[1])[1]
+
+                    # iterate over the numbers; if one is succesful break all loops and add 1 to the sum
                     for i in range(0, len(l)-1):
                         difference, is_decr = level_difference_and_is_decreasing(l[i], l[i+1])
 
-                        if (difference >= 4 or difference <= 0) or (is_decr_initial_value != is_decr) and (removed_one_number):
+                        if (difference >= 4 or difference <= 0) or (is_decr_initial_value != is_decr):
                             save_with_removed_number = False
                             break
 
