63f848bce5ef9b751553f1af145f82726eb6c448
Jonas Gilbert <jonas.gilbert.0503@gmail.com>
2025-01-05 19:15:54 +0100

    Day 3 completed

--- 2024/code/day3.py
+++ 2024/code/day3.py
@@ -13,18 +13,27 @@ def get_all_valid_multiplications_part1():
     return valid_multiplications
 
 
-# TODO: Not working
+# remove all new lines to get one long string; a don't() can effect the next line too
 def get_all_valid_multiplications_part2():
-    file = open('./input/input3.txt', 'r')
-    s = file.readline()
+    # remove new lines
+    content = open('./input/input3.txt', 'r').read()
+    updated_content = content.replace('\n', '')
+
+    # remove and write content
+    updated_file = open('./input/input3.txt', 'w')
+    updated_file.truncate(0)
+    updated_file.write(updated_content)
+    updated_file.close()
+
+    # read updated content
+    updated_file = open('./input/input3.txt', 'r')
+
+    content = updated_file.readline()
     valid_multiplications = []
     all_substrings = []
 
-    while s:
-        all_substrings.extend(re.split(r'(don\'t\(\).*?do\(\)|don\'t\(\).*?$)', s))
-        s = file.readline()
-
-    print(all_substrings)
+    # remove all content with a starting don't() and an ending do() or line end
+    all_substrings = re.split(r'don\'t\(\).*?do\(\)|don\'t\(\).*?$', content)
 
     for substring in all_substrings:
         valid_multiplications.extend(re.findall(r'mul\(\d{1,3},\d{1,3}\)', substring))
